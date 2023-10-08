08b010fc43a1eeb5170e401e5cb715d5f5ff211a
claram97 <cruano@fi.uba.ar>
2023-10-07 22:25:29 -0300

    manejo de errores en directories.rs

--- src/directories.rs
+++ src/directories.rs
@@ -15,6 +15,6 @@ pub fn create_directory(name: &str) {
 pub fn store_file(path: &str) -> io::Result<String> {
     let content_hash = hash_object::hash_file_content(path)?;
     let file_path = Path::new("objects").join(&content_hash);
-    fs::copy(path, file_path).expect("Unable to copy file");
+    fs::copy(path, file_path)?;
     Ok(content_hash)
 }
