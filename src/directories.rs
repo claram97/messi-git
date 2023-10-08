aadb6b88d8bd7b93614db8c016d8f5e2c418632f
Clara Ruano Frugoli <cruano@fi.uba.ar>
2023-10-07 23:23:20 -0300

    directories solo crea directorios

--- src/directories.rs
+++ src/directories.rs
@@ -1,8 +1,5 @@
-use std::{path::Path, fs::{self, File}, io::{self}};
+use std::{path::Path, fs::{self}};
 
-use flate2::{write::ZlibEncoder, Compression};
-
-use crate::hash_object;
 
 pub fn create_directory(name: &str) {
     let path = Path::new(name);
@@ -14,19 +11,3 @@ pub fn create_directory(name: &str) {
     }
 }
 
-fn compress_content(input_path: &str, output_path: &str) -> io::Result<()> {
-    let mut input_file = File::open(input_path)?;
-    let output_file = File::create(output_path)?;
-    let mut encoder = ZlibEncoder::new(output_file, Compression::default());
-
-    std::io::copy(&mut input_file, &mut encoder)?;
-    encoder.finish()?;
-    Ok(())
-}
-
-pub fn store_file(path: &str) -> io::Result<String> {
-    let content_hash = hash_object::hash_file_content(path)?;
-    let output_file_str = "objects/".to_string() + &content_hash;
-    compress_content(path, output_file_str.as_str())?;
-    Ok(content_hash)
-}
