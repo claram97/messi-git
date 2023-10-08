e28fcb4cd3575d436e56074ebe0f2ba8ce531658
Clara Ruano Frugoli <cruano@fi.uba.ar>
2023-10-07 23:23:01 -0300

    hash object comprime y hace el store file

--- src/hash_object.rs
+++ src/hash_object.rs
@@ -1,6 +1,7 @@
-use std::io;
+use std::{io, fs::File};
 
 use sha1::{Digest, Sha1};
+use flate2::{write::ZlibEncoder, Compression};
 
 pub fn hash_string(content: &str) -> String {
     let mut hasher = Sha1::new();
@@ -12,4 +13,21 @@ pub fn hash_string(content: &str) -> String {
 pub fn hash_file_content(path: &str) -> io::Result<String> {
     let content = std::fs::read_to_string(path)?;
     Ok(hash_string(&content))
+}
+
+pub fn store_file(path: &str) -> io::Result<String> {
+    let content_hash = hash_file_content(path)?;
+    let output_file_str = "objects/".to_string() + &content_hash;
+    compress_content(path, output_file_str.as_str())?;
+    Ok(content_hash)
+}
+
+fn compress_content(input_path: &str, output_path: &str) -> io::Result<()> {
+    let mut input_file = File::open(input_path)?;
+    let output_file = File::create(output_path)?;
+    let mut encoder = ZlibEncoder::new(output_file, Compression::default());
+
+    std::io::copy(&mut input_file, &mut encoder)?;
+    encoder.finish()?;
+    Ok(())
 }
