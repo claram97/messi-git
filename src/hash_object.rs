3d25a643e9162b902515be62ce68f6ea93d76643
fe-martinez <femartinez@fi.uba.ar>
2023-10-10 19:16:14 -0300

    carpeta dentro de objects
    ahora los primeros 2 caracteres del resultado de hashear el archivo
    son el nombre de la carpeta donde va este

--- src/hash_object.rs
+++ src/hash_object.rs
@@ -1,4 +1,4 @@
-use std::{io::{self, Write}, fs::File};
+use std::{io::{self, Write}, fs::{File, self}, path::Path};
 
 use sha1::{Digest, Sha1};
 use flate2::{write::ZlibEncoder, Compression};
@@ -17,9 +17,26 @@ pub fn hash_file_content(path: &str) -> io::Result<String> {
     Ok(hash_string(&complete))
 }
 
+fn create_directory(name: &str) {
+    let path = Path::new(name);
+    if !path.exists() {
+        match fs::create_dir(path) {
+            Err(why) => panic!("couldn't create directory: {}", why),
+            Ok(_) => println!("created directory"),
+        }
+    }
+}
+
+/// Stores the file at the given path in the objects directory.
+/// Returns the hash of the file content.
+/// 
+/// Stores the file in the path: objects/<first 2 characters of hash>/<remaining characters of hash>
+/// The file is compressed using zlib.
 pub fn store_file(path: &str) -> io::Result<String> {
     let content_hash = hash_file_content(path)?;
-    let output_file_str = "objects/".to_string() + &content_hash;
+    let output_file_dir = "objects/".to_string() + &content_hash[..2] + "/";
+    create_directory(&output_file_dir);
+    let output_file_str = output_file_dir + &content_hash[2..];
     compress_content(path, output_file_str.as_str())?;
     Ok(content_hash)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use messi::{directories::create_directory, hash_object};
+
 fn main() {
-    println!("Hello, World!)
+    println!("Hello, World!");
+
+    create_directory("objects");
+
+    let result = hash_object::store_file("src/hash_object.rs");
+    println!("{:?}", result)
 }
