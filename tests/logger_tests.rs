f95999ae4566c03ed607e2b88cda7c429eec8ed2
Ramiro Gestoso <rgestoso@fi.uba.ar>
2023-10-10 17:36:45 -0300

    agrego mas tests

--- tests/logger_tests.rs
+++ tests/logger_tests.rs
@@ -7,10 +7,41 @@ fn test_write_single() -> std::io::Result<()> {
     let path = "tests/logs/log.txt";
     let content = "testing log";
     let mut logger = Logger::new(path)?;
-    dbg!("ACA");
+
     write!(logger, "{content}")?;
     let file_content = fs::read_to_string(path)?;
     assert_eq!(file_content, content);
+
+    logger.clear()
+}
+
+#[test]
+fn test_write_single_many_times() -> std::io::Result<()> {
+    let path = "tests/logs/log.txt";
+    let content = "testing log";
+    let mut logger = Logger::new(path)?;
+
+    writeln!(logger, "{content}")?;
+    writeln!(logger, "{content}")?;
+    writeln!(logger, "{content}")?;
+    
+    for line_content in fs::read_to_string(path)?.lines() {
+        assert_eq!(line_content, content);
+    }
+
+    Ok(())
+}
+
+#[test]
+fn test_write_and_clear() -> std::io::Result<()> {
+    let path = "tests/logs/log.txt";
+    let content = "testing log";
+    let mut logger = Logger::new(path)?;
+
+    writeln!(logger, "{content}")?;
+    logger.clear()?;
+    let file_content = fs::read_to_string(path)?;
+    assert_eq!(file_content, "");
 
     Ok(())
 }
