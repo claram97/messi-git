4c3c4cdbb873c3157605b571ceb345decda48b86
fe-martinez <femartinez@fi.uba.ar>
2023-10-11 19:08:06 -0300

    delete directories.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-use messi::{directories::create_directory, hash_object};
+use messi::hash_object;
 
 fn main() {
     println!("Hello, World!");
-
-    create_directory("objects");
 
     let result = hash_object::store_file("src/hash_object.rs");
     println!("{:?}", result)
