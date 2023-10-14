c6bcb492e4c4bf18a09b26ac89057414f1836f10
paulabruck <pbruck.ext@fi.uba.ar>
2023-10-14 20:56:44 -0300

    comienzo de chequear parametros

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let args = get_user_input();
+    let second_argument = match args.get(1) {
+        Some(arg) => arg,
+        None => {
+            eprintln!("No se ha ingresado el tercer argumento.");
+            return;
+        }
+    };
+
+    println!("Tercer argumento ingresado: {}", second_argument);
+}
+
+fn get_user_input() -> Vec<String> {
+    let mut input = String::new();
+    println!("Ingresa los argumentos (separados por espacios):");
+    io::stdin().read_line(&mut input).expect("Error al leer la entrada del usuario");
+    
+    // Divide la entrada en argumentos y la almacena en un vector
+    let args: Vec<String> = input.trim().split_whitespace().map(|s| s.to_string()).collect();
+    args
 }
