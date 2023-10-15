d38df80283fcdc6777648b1128fb74ba83fb8812
paulabruck <pbruck.ext@fi.uba.ar>
2023-10-14 21:42:43 -0300

    modularizar

--- src/main.rs
+++ src/main.rs
@@ -20,6 +20,69 @@ enum GitCommand {
     Branch,
 }
 
+fn get_user_input() -> Vec<String> {
+    let mut input = String::new();
+    println!("Ingresa los argumentos (separados por espacios):");
+    if io::stdin().read_line(&mut input).is_err() {
+        eprintln!("Error al leer la entrada del usuario");
+    }
+    
+    let args: Vec<String> = input.trim().split_whitespace().map(|s| s.to_string()).collect();
+    args
+}
+
+fn parse_git_command(second_argument: &str) -> Option<GitCommand> {
+    match second_argument {
+        "hash-object" => Some(GitCommand::HashObject),
+        "cat-file" => Some(GitCommand::CatFile),
+        "init" => Some(GitCommand::Init),
+        "status" => Some(GitCommand::Status),
+        "add" => Some(GitCommand::Add),
+        "rm" => Some(GitCommand::Rm),
+        "commit" => Some(GitCommand::Commit),
+        "checkout" => Some(GitCommand::Checkout),
+        "log" => Some(GitCommand::Log),
+        "clone" => Some(GitCommand::Clone),
+        "fetch" => Some(GitCommand::Fetch),
+        "merge" => Some(GitCommand::Merge),
+        "remote" => Some(GitCommand::Remote),
+        "pull" => Some(GitCommand::Pull),
+        "push" => Some(GitCommand::Push),
+        "branch" => Some(GitCommand::Branch),
+        _ => {
+            eprintln!("No es una opción válida de Git.");
+            None
+        }
+    }
+}
+
+fn handle_git_command(git_command: GitCommand, second_argument: &str) {
+    match git_command {
+        GitCommand::HashObject
+        | GitCommand::CatFile
+        | GitCommand::Status
+        | GitCommand::Add
+        | GitCommand::Rm
+        | GitCommand::Commit
+        | GitCommand::Checkout
+        | GitCommand::Log
+        | GitCommand::Clone
+        | GitCommand::Fetch
+        | GitCommand::Merge
+        | GitCommand::Remote
+        | GitCommand::Pull
+        | GitCommand::Push
+        | GitCommand::Branch => {
+            println!("Segundo argumento ingresado: {}", second_argument);
+        }
+        GitCommand::Init => {
+            println!("Segundo argumento ingresado: {}", second_argument);
+            if let Err(err) = git_init("a", "branch", None) {
+                eprintln!("Error al inicializar el repositorio Git: {}", err);
+            }
+        }
+    }
+}
 
 fn main() {
     let args = get_user_input();
@@ -31,93 +94,8 @@ fn main() {
         }
     };
 
-    let git_command = match second_argument.as_str() {
-        "hash-object" => GitCommand::HashObject,
-        "cat-file" => GitCommand::CatFile,
-        "init" => GitCommand::Init,
-        "status" => GitCommand::Status,
-        "add" => GitCommand::Add,
-        "rm" => GitCommand::Rm,
-        "commit" => GitCommand::Commit,
-        "checkout" => GitCommand::Checkout,
-        "log" => GitCommand::Log,
-        "clone" => GitCommand::Clone,
-        "fetch" => GitCommand::Fetch,
-        "merge" => GitCommand::Merge,
-        "remote" => GitCommand::Remote,
-        "pull" => GitCommand::Pull,
-        "push" => GitCommand::Push,
-        "branch" => GitCommand::Branch,
-        _ => {
-            eprintln!("No es una opción válida de Git.");
-            return; 
-        }
-    };
-
-    match git_command {
-        GitCommand::HashObject => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::CatFile => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Init => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-            if let Err(err) = git_init("a", "branch", None) {
-                eprintln!("Error al inicializar el repositorio Git: {}", err);
-            } 
-        }
-        GitCommand::Status => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Add => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Rm => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Commit => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Checkout => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Log => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Clone => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Fetch => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Merge => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Remote => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Pull => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Push => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        },
-         _ => todo!()
-       
+    if let Some(git_command) = parse_git_command(second_argument) {
+        handle_git_command(git_command, second_argument);
     }
 }
 
-
-
-
-fn get_user_input() -> Vec<String> {
-    let mut input = String::new();
-    println!("Ingresa los argumentos (separados por espacios):");
-    if io::stdin().read_line(&mut input).is_err() {
-        eprintln!("Error al leer la entrada del usuario");
-    }
-    
-    let args: Vec<String> = input.trim().split_whitespace().map(|s| s.to_string()).collect();
-    args
-}
