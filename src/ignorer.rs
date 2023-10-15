92c38bfe065d45b0e7ac939176fa8fdf9097f0cc
Ramiro Gestoso <rgestoso@fi.uba.ar>
2023-10-15 13:28:15 -0300

    ignorer docu

--- src/ignorer.rs
+++ src/ignorer.rs
@@ -2,12 +2,18 @@ use std::fs;
 
 const MGIT_IGNORE: &str = ".mgitignore";
 
+
+/// This is a helper structure that will help some git commands
+/// to know if a path has to be ignored or not according to
+/// the content of git ignore file.
 #[derive(Default)]
 pub struct Ignorer {
     paths: Vec<String>,
 }
 
 impl Ignorer {
+    /// This method loads the git ignore file and returns an
+    /// Ignorer ready to use
     pub fn load() -> Self {
         match fs::read_to_string(MGIT_IGNORE) {
             Ok(file) => Self {
@@ -17,6 +23,7 @@ impl Ignorer {
         }
     }
 
+    /// This method will decide whether a path has to be ignored or not
     pub fn ignore(&self, path: &str) -> bool {
         for ignored in &self.paths {
             if is_subpath(path, ignored) {
@@ -33,6 +40,8 @@ fn get_subpaths(path: &str) -> Vec<&str> {
         .collect()
 }
 
+/// This is a helper function that can be util also outside Ignorer
+/// Checks if a path is a subpath of the other
 pub fn is_subpath(subpath: &str, path: &str) -> bool {
     let path_parent: Vec<&str> = get_subpaths(path);
     let path_child: Vec<&str> = get_subpaths(subpath);
