f5dd661ae5f0b215b2765029ef87ca647b29b2a2
paulabruck <pbruck.ext@fi.uba.ar>
2023-10-14 21:10:06 -0300

    agrego ejecutar funcion si hay comando

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,85 @@
 use std::io;
 
+enum GitCommand {
+    HashObject,
+    CatFile,
+    Init,
+    Status,
+    Add,
+    Rm,
+    Commit,
+    Checkout,
+    Log,
+    Clone,
+    Fetch,
+    Merge,
+    Remote,
+    Pull,
+    Push,
+    Branch,
+}
+
+
 fn main() {
     let args = get_user_input();
     let second_argument = match args.get(1) {
         Some(arg) => arg,
         None => {
-            eprintln!("No se ha ingresado el tercer argumento.");
-            return;
+            eprintln!("No se ha ingresado el segundo argumento.");
+            return; 
         }
     };
 
-    println!("Tercer argumento ingresado: {}", second_argument);
+    let git_command = match second_argument.as_str() {
+        "hash-object" => GitCommand::HashObject,
+        "cat-file" => GitCommand::CatFile,
+        "init" => GitCommand::Init,
+       
+        _ => {
+            eprintln!("El segundo argumento no es una opción válida de Git.");
+            return; 
+        }
+    };
+
+    match git_command {
+        GitCommand::HashObject => {
+            println!("Segundo argumento ingresado: {}", second_argument);
+            execute_hash_object(); 
+        }
+        GitCommand::CatFile => {
+            println!("Segundo argumento ingresado: {}", second_argument);
+            execute_cat_file(); 
+        }
+        GitCommand::Init => {
+            println!("Segundo argumento ingresado: {}", second_argument);
+            execute_init(); 
+        },
+         _ => todo!()
+       
+    }
 }
+
+fn execute_hash_object() {
+    println!("Ejecutando 'git hash-object'...");
+    
+}
+
+fn execute_cat_file() {
+    println!("Ejecutando 'git cat-file'...");
+}
+
+fn execute_init() {
+    println!("Ejecutando 'git init'...");
+}
+
 
 fn get_user_input() -> Vec<String> {
     let mut input = String::new();
     println!("Ingresa los argumentos (separados por espacios):");
-    io::stdin().read_line(&mut input).expect("Error al leer la entrada del usuario");
+    if io::stdin().read_line(&mut input).is_err() {
+        eprintln!("Error al leer la entrada del usuario");
+    }
     
-    // Divide la entrada en argumentos y la almacena en un vector
     let args: Vec<String> = input.trim().split_whitespace().map(|s| s.to_string()).collect();
     args
 }
