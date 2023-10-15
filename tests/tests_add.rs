4d4102beab6f70d805b2c3141fef36f7f46d64ac
Ramiro Gestoso <rgestoso@fi.uba.ar>
2023-10-15 13:03:17 -0300

    tests de integracion

---
+++ tests/tests_add.rs
@@ -0,0 +1,70 @@
+use std::fs;
+use std::io::{self, Write};
+use messi::{add, index};
+
+fn write_file(path: &str, content: &str) -> io::Result<()> {
+    let mut file = fs::File::create(path)?;
+    write!(file, "{}", content)
+}
+
+#[test]
+fn test_add_file_is_in_index() -> io::Result<()>{
+    let index_path = ".mgit/index1";
+    write_file(index_path, "")?;
+    // Given a file with a path
+    let path = "tests/add/dir_to_add/non_empty/a.txt";
+    write_file(path, "")?;
+
+    // When added to staging area
+    add::add(path, index_path, None)?;
+
+    // Then it is saved in index file
+    let index = index::Index::load(index_path)?;
+    assert!(index.contains(path));
+
+    Ok(())
+}
+
+#[test]
+fn test_update_file() -> io::Result<()> {
+    let index_path = ".mgit/index2";
+    write_file(index_path, "")?;
+    // Given a file with a path
+    let path = "tests/add/dir_to_add/non_empty/b.txt";
+    write_file(path, "a new file!")?;
+
+    // When added to staging area
+    add::add(path, index_path, None)?;
+    // And after that it is modified
+    write_file(path, "an updated file!")?;
+    let index = index::Index::load(index_path)?;
+    let first_hash = index.get_hash(path).unwrap();
+    // And added again
+    add::add(path, index_path, None)?;
+    let index = index::Index::load(index_path)?;
+    let updated_hash = index.get_hash(path).unwrap();
+
+    // Then its hash is updated
+    assert_ne!(first_hash, updated_hash);
+
+    Ok(())
+}
+
+#[test]
+fn test_removing_file() -> io::Result<()> {
+    let index_path = ".mgit/index3";
+    write_file(index_path, "")?;
+    // Given a file with a path
+    let path = "tests/add/dir_to_add/non_empty/d.txt";
+    write_file(path, "a new file!")?;
+    // When added to staging area
+    add::add(path, index_path, None)?;
+    // And after it is deleted
+    fs::remove_file(path)?;
+    // And added again
+    add::add(path, index_path, None)?;
+    // Then the file is no longer in staging area
+    let index = index::Index::load(index_path)?;
+    assert!(!index.contains(path));
+    Ok(())
+}
