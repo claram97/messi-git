7fd8d5c9ac85fe214bb9297dda989e60b7070aee
paulabruck <pbruck.ext@fi.uba.ar>
2023-10-14 21:54:24 -0300

    parsear cada comando

--- src/parse_commands.rs
+++ src/parse_commands.rs
@@ -58,28 +58,94 @@ pub fn parse_git_command(second_argument: &str) -> Option<GitCommand> {
 
 pub fn handle_git_command(git_command: GitCommand, second_argument: &str) {
     match git_command {
-        GitCommand::HashObject
-        | GitCommand::CatFile
-        | GitCommand::Status
-        | GitCommand::Add
-        | GitCommand::Rm
-        | GitCommand::Commit
-        | GitCommand::Checkout
-        | GitCommand::Log
-        | GitCommand::Clone
-        | GitCommand::Fetch
-        | GitCommand::Merge
-        | GitCommand::Remote
-        | GitCommand::Pull
-        | GitCommand::Push
-        | GitCommand::Branch => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-        }
-        GitCommand::Init => {
-            println!("Segundo argumento ingresado: {}", second_argument);
-            if let Err(err) = git_init("a", "branch", None) {
-                eprintln!("Error al inicializar el repositorio Git: {}", err);
-            }
-        }
+        GitCommand::HashObject => handle_hash_object(second_argument),
+        GitCommand::CatFile => handle_cat_file(second_argument),
+        GitCommand::Status => handle_status(second_argument),
+        GitCommand::Add => handle_add(second_argument),
+        GitCommand::Rm => handle_rm(second_argument),
+        GitCommand::Commit => handle_commit(second_argument),
+        GitCommand::Checkout => handle_checkout(second_argument),
+        GitCommand::Log => handle_log(second_argument),
+        GitCommand::Clone => handle_clone(second_argument),
+        GitCommand::Fetch => handle_fetch(second_argument),
+        GitCommand::Merge => handle_merge(second_argument),
+        GitCommand::Remote => handle_remote(second_argument),
+        GitCommand::Pull => handle_pull(second_argument),
+        GitCommand::Push => handle_push(second_argument),
+        GitCommand::Branch => handle_branch(second_argument),
+        GitCommand::Init => handle_init(second_argument),
     }
 }
+
+fn handle_hash_object(second_argument: &str) {
+    println!("Handling HashObject command with argument: {}", second_argument);
+}
+
+fn handle_cat_file(second_argument: &str) {
+    println!("Handling CatFile command with argument: {}", second_argument);
+}
+fn handle_status(second_argument: &str) {
+    println!("Handling Status command with argument: {}", second_argument);
+}
+
+fn handle_add(second_argument: &str) {
+    println!("Handling Add command with argument: {}", second_argument);
+
+}
+
+fn handle_rm(second_argument: &str) {
+    println!("Handling Rm command with argument: {}", second_argument);
+}
+
+fn handle_commit(second_argument: &str) {
+    println!("Handling Commit command with argument: {}", second_argument);
+}
+
+fn handle_checkout(second_argument: &str) {
+    println!("Handling Checkout command with argument: {}", second_argument);
+}
+
+fn handle_log(second_argument: &str) {
+    println!("Handling Log command with argument: {}", second_argument);
+
+}
+
+fn handle_clone(second_argument: &str) {
+    println!("Handling Clone command with argument: {}", second_argument);
+    
+}
+
+fn handle_fetch(second_argument: &str) {
+    println!("Handling Fetch command with argument: {}", second_argument);
+    
+}
+
+fn handle_merge(second_argument: &str) {
+    println!("Handling Merge command with argument: {}", second_argument);
+    
+}
+
+fn handle_remote(second_argument: &str) {
+    println!("Handling Remote command with argument: {}", second_argument);
+    
+}
+
+fn handle_pull(second_argument: &str) {
+    println!("Handling Pull command with argument: {}", second_argument);
+    
+}
+
+fn handle_push(second_argument: &str) {
+    println!("Handling Push command with argument: {}", second_argument);
+    
+}
+
+fn handle_branch(second_argument: &str) {
+    println!("Handling Branch command with argument: {}", second_argument);
+}
+
+fn handle_init(second_argument: &str) {
+    println!("Handling Init command with argument: {}", second_argument);
+    
+}
+
