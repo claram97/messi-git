daeff8f4f60b2faf212a2ae46194c4d600e72c2a
Clara Ruano Frugoli <cruano@fi.uba.ar>
2023-10-16 11:26:12 -0300

    hago cambios en la función para hacerla testeable

--- src/utils.rs
+++ src/utils.rs
@@ -1,36 +1,43 @@
 pub(crate) const NAME_OF_GIT_DIRECTORY: &str = ".git";
 pub(crate) const NAME_OF_INDEX_FILE: &str = "index-file";
-/**
- * Recursively searches for the Git directory starting from the current directory and moving upwards.
- *
- * # Example
- *
- * ```
- * use std::env;
- *
- * let git_directory = find_git_directory();
- * match git_directory {
- *     Some(dir) => println!("Git directory found at: {}", dir),
- *     None => println!("No Git directory found in the path."),
- * }
- * ```
- *
- * The function returns `Some(path)` if a valid Git directory is found, where `path` is the path to the found Git directory.
- * If no Git directory is found anywhere in the path upwards from the current directory, the function returns `None`.
- */
-pub fn find_git_directory() -> Option<String> {
-    if let Ok(current_dir) = env::current_dir() {
-        let mut current_dir = current_dir;
+/// Recursively searches for a directory named "name_of_git_directory" in the file system
+/// starting from the location specified by "current_dir."
+///
+/// # Arguments
+///
+/// * `current_dir`: A mutable reference to a `PathBuf` representing the initial location from which the search begins.
+/// * `name_of_git_directory`: The name of the directory being sought.
+///
+/// # Returns
+///
+/// This function returns an `Option<String>` containing the path to the found directory as a string if it is found.
+/// If the directory is not found, it returns `None`.
+///
+/// # Example
+///
+/// ```
+///
+/// let mut current_dir = env::current_dir();
+/// let git_directory_name = ".git";
+///
+/// if let Some(git_dir_path) = find_git_directory(&mut current_dir, git_directory_name) {
+///     println!("Found the Git directory at: {}", git_dir_path);
+/// } else {
+///     println!("Git directory not found in the search path.");
+/// }
+/// ```
+pub fn find_git_directory(
+    current_dir: &mut PathBuf,
+    name_of_git_directory: &str,
+) -> Option<String> {
+    loop {
+        let git_dir = current_dir.join(name_of_git_directory);
+        if git_dir.exists() && git_dir.is_dir() {
+            return Some(git_dir.display().to_string());
+        }
 
-        loop {
-            let git_dir = current_dir.join(NAME_OF_GIT_DIRECTORY);
-            if git_dir.exists() && git_dir.is_dir() {
-                return Some(git_dir.display().to_string());
-            }
-
-            if !current_dir.pop() {
-                break;
-            }
+        if !current_dir.pop() {
+            break;
         }
     }
 
