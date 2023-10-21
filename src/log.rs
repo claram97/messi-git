532768a719785387fe090ca1720230e12fca4a17
Ramiro Gestoso <rgestoso@fi.uba.ar>
2023-10-21 18:21:06 -0300

    refactores

--- src/log.rs
+++ src/log.rs
@@ -2,7 +2,7 @@ use crate::cat_file;
 use std::{
     fmt::Display,
     fs,
-    io::{self, Error}
+    io::{self, Error}, path::Path
 };
 
 /// LogIter is a structure that will help to iterate
@@ -79,7 +79,7 @@ impl Log {
     /// The load of the Log may fail because of I/O errors.
     pub fn load(commit: Option<&str>, git_dir: &str) -> io::Result<Self> {
         if let Some(hash) = commit {
-            Self::new_from_hash(hash, git_dir)
+            Self::load_from_hash(hash, git_dir)
         } else {
             Self::load_from_head(git_dir)
         }
@@ -89,41 +89,37 @@ impl Log {
         let head_path = format!("{}/HEAD", git_dir);
         let head_content = fs::read_to_string(head_path)?;
         let last_commit_ref = head_content.trim().split(": ").last();
-        if let Some(commit_ref) = last_commit_ref {
-            let heads_path = format!("{}/{}", git_dir, commit_ref);
-            dbg!(&heads_path);
-            match fs::read_to_string(heads_path) {
-                Ok(hash) => Self::new_from_hash(&hash.trim(), git_dir),
-                Err(_) => Self::new_from_hash(commit_ref, git_dir),
-            }
-            // if Path::new(&heads_path).exists() {
-            //     let hash = fs::read_to_string(heads_path)?;
-            //     Self::new_from_hash(&hash, git_dir)
-            // } else {
-            //     dbg!(&commit_ref);
-            //     Self::new_from_hash(commit_ref, git_dir)
-            // }
-        } else {
-            Err(invalid_data_error(&head_content))
+
+        match last_commit_ref {
+            Some(refs) => {
+                let heads_path = format!("{}/{}", git_dir, refs);
+                if Path::new(&heads_path).exists() {
+                    let hash = fs::read_to_string(heads_path)?;
+                    Self::load_from_hash(&hash.trim(), git_dir)
+                } else {
+                    Self::load_from_hash(refs, git_dir)
+                }
+            },
+            None => Err(invalid_data_error(&head_content)),
         }
     }
 
-    fn new_from_hash(hash: &str, git_dir: &str) -> io::Result<Self> {
+    fn load_from_hash(hash: &str, git_dir: &str) -> io::Result<Self> {
         let commit_content = cat_file::cat_file_return_content(hash, git_dir)?;
-        let mut log = Self::default();
-        log.git_dir = git_dir.to_string();
-        log.commit_hash = hash.to_string();
-
         let header_lines = commit_content.lines().position(|line| line.is_empty());
-
-        if let Some(n) = header_lines {
-            for line in commit_content.lines().take(n) {
-                log.parse_commit_header_line(line)?;
-            }
-            log.message = commit_content.lines().skip(n).collect();
-            Ok(log)
-        } else {
-            Err(invalid_data_error(hash))
+        
+        match header_lines {
+            Some(n) => {
+                let mut log = Self::default();
+                for line in commit_content.lines().take(n) {
+                    log.parse_commit_header_line(line)?;
+                }
+                log.message = commit_content.lines().skip(n).collect();
+                log.git_dir = git_dir.to_string();
+                log.commit_hash = hash.to_string();
+                Ok(log)
+            },
+            None => Err(invalid_data_error(hash)),
         }
     }
 
@@ -148,7 +144,7 @@ impl Log {
 
     fn get_parent_log(&self) -> Option<Self> {
         if let Some(parent) = &self.parent_hash {
-            let next_log = Log::new_from_hash(&parent, &self.git_dir);
+            let next_log = Log::load_from_hash(&parent, &self.git_dir);
             match next_log {
                 Ok(mut log) => {
                     log.oneline = self.oneline;
@@ -214,7 +210,7 @@ mod tests {
 
     #[test]
     fn test() {
-        let log = Log::new_from_hash("c6e4695d7f410a8c49787c7c87c5b390b56dc53a", ".git");
+        let log = Log::load_from_hash("c6e4695d7f410a8c49787c7c87c5b390b56dc53a", ".git");
         assert!(log.is_ok())
     }
 
