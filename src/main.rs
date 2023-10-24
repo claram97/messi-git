2b1f15e42161a90b053518310772f354bd6e56e2
paulabruck <paulabruck@gmail.com>
2023-10-24 19:07:43 +0000

    ajustes botones

--- src/main.rs
+++ src/main.rs
@@ -19,10 +19,38 @@ fn main() {
     let window: gtk::Window = builder.get_object("window").expect("No se puede obtener la ventana");
     window.set_default_size(800, 600); 
     let button: gtk::Button = builder.get_object("buttonclone").expect("No se puede obtener el botón");
+    let button_init: gtk::Button = builder.get_object("buttoninit").expect("No se puede obtener el botón Init");
 
-    // Modifica las propiedades del botón
-    button.set_label("Nuevo Texto"); // Cambia el texto del botón
-   
+    let label = button.get_child().unwrap().downcast::<gtk::Label>().unwrap();
+    let pango_desc = pango::FontDescription::from_string("Sans 30"); // Cambia el tamaño de fuente a 14
+    label.override_font(&pango_desc);
+
+    let label1 = button_init.get_child().unwrap().downcast::<gtk::Label>().unwrap();
+    let pango_desc = pango::FontDescription::from_string("Sans 30"); // Cambia el tamaño de fuente a 14
+    label1.override_font(&pango_desc);
+
+     // Aplicar un estilo de fondo al botón
+     let css_provider = gtk::CssProvider::new();
+     css_provider
+         .load_from_data("button {
+            background-color: #87CEEB; /* Color celeste */
+             color: #1e3799; /* Color de texto blanco */
+             border: 10px solid #1e3799; /* Borde azul oscuro */
+             padding: 10px; /* Espaciado alrededor del contenido */
+         }"
+         .as_bytes())
+         .expect("Failed to load CSS");
+ 
+     let style_context = button.get_style_context();
+     style_context.add_provider(&css_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
+
+     let style_context_init = button_init.get_style_context();
+     style_context_init.add_provider(&css_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
+ 
+ 
+     button.show();
+     button_init.show();
+
     
     window.show_all();
 
