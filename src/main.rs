dd1d8eb23e4dae23b7415c5cf831ee0b031a2482
paulabruck <paulabruck@gmail.com>
2023-10-24 15:55:49 +0000

    primera prueba con glade

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,23 @@
-extern crate messi;
-use messi::gui::run_gui;
+// extern crate messi;
+// use messi::gui::run_gui;
+
+// fn main() {
+//    // run_gui();
+// }
+use gtk::prelude::*;
+use gtk::Builder;
 
 fn main() {
-    run_gui();
+    if gtk::init().is_err() {
+        println!("Failed to initialize GTK.");
+        return;
+    }
+
+    let builder = Builder::new();
+    builder.add_from_file("src/part3.ui"); // Reemplaza con el nombre de tu archivo .ui
+
+    let window: gtk::Window = builder.get_object("window").expect("No se puede obtener la ventana");
+    window.show_all();
+
+    gtk::main();
 }
