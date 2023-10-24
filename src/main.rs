1e079e3f46f0f1e1d7abba4e5f6f11864d4a85ab
paulabruck <paulabruck@gmail.com>
2023-10-24 18:05:26 +0000

    ajustes tam

--- src/main.rs
+++ src/main.rs
@@ -17,6 +17,13 @@ fn main() {
     builder.add_from_file("src/part3.ui"); // Reemplaza con el nombre de tu archivo .ui
 
     let window: gtk::Window = builder.get_object("window").expect("No se puede obtener la ventana");
+    window.set_default_size(800, 600); 
+    let button: gtk::Button = builder.get_object("buttonclone").expect("No se puede obtener el botón");
+
+    // Modifica las propiedades del botón
+    button.set_label("Nuevo Texto"); // Cambia el texto del botón
+   
+    
     window.show_all();
 
     gtk::main();
