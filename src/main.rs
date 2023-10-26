ef6f7db7e66ee22cbf8e22faeb87559ab3071a4a
paulabruck <paulabruck@gmail.com>
2023-10-26 16:13:36 +0000

    init winodw

--- src/main.rs
+++ src/main.rs
@@ -30,82 +30,18 @@ fn connect_button_clicked(button: &gtk::Button, button_type: &str) {
     let button_type = button_type.to_owned(); // Clonar la etiqueta
     
     button.connect_clicked(move |_| {
-        let new_window = gtk::Window::new(gtk::WindowType::Toplevel);
-       
-
-        match &button_type[..] {
-            "Clone" => {
-                // Acciones específicas para el botón "Clone"
-                println!("Botón 'Clone' presionado");
-                new_window.set_title("Clone Repository");
-            }
-            "Init" => {
-                // Acciones específicas para el botón "Init"
-                println!("Botón 'Init' presionado");
-                new_window.set_title("Init  Repository");
-                create_init_options_buttons(&new_window);
-            }
-            _ => {
-                // Manejar otros casos si es necesario
-            }
+        if button_type == "Init" {
+            let builder_window_init = gtk::Builder::new();
+            builder_window_init.add_from_file("src/windowInit.ui"); // Asegúrate de que la ruta sea correcta
+            let new_window_init: gtk::Window = builder_window_init.get_object("window").expect("No se puede obtener la ventana");
+            new_window_init.set_default_size(800, 600);
+            // Mostrar la nueva ventana "Init"
+            new_window_init.show_all();
+        } else if button_type == "Clone" {
+            // Código para el botón "Clone" aquí
         }
-        new_window.set_default_size(800, 600);
-        apply_window_style(&new_window); 
-        new_window.show_all();
     });
 }
-
-fn create_init_options_buttons(parent_window: &gtk::Window) {
-    let container = gtk::Box::new(gtk::Orientation::Vertical, 10);
-
-    let container = gtk::Box::new(gtk::Orientation::Vertical, 10);
-
-    // Crear botones para las opciones de git init
-    let button_option1 = gtk::Button::with_label("Git init");
-    let button_option2 = gtk::Button::with_label("Git init -b <branch>");
-    let button_option3 = gtk::Button::with_label("Git init --template <path>");
-
-    // Aplicar estilos CSS personalizados a los botones
-    let css_provider = gtk::CssProvider::new();
-    css_provider.load_from_data("button {
-        background-color: #87CEEB; /* Color celeste */
-        color: #1e3799; /* Color de texto azul oscuro */
-        border: 10px solid #1e3799; /* Borde azul oscuro */
-        padding: 10px; /* Espaciado alrededor del contenido */
-    }"
-        .as_bytes(),
-    )
-    .expect("Failed to load CSS");
-
-    let style_context_option1 = button_option1.get_style_context();
-    style_context_option1.add_provider(&css_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
-
-    let style_context_option2 = button_option2.get_style_context();
-    style_context_option2.add_provider(&css_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
-
-    let style_context_option3 = button_option3.get_style_context();
-    style_context_option3.add_provider(&css_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
- container.pack_start(&button_option1, true, true, 0);
-    container.pack_start(&button_option2, true, true, 0);
-    container.pack_start(&button_option3, true, true, 0);
-
-    // Conectar acciones a los botones
-    button_option1.connect_clicked(|_| {
-        //run_git_init("");
-    });
-
-    button_option2.connect_clicked(|_| {
-        // Implementa la lógica para git init -b <branch>
-    });
-
-    button_option3.connect_clicked(|_| {
-        // Implementa la lógica para git init --template <path>
-    });
-
-    parent_window.add(&container);
-    container.show_all();
-}
-
 
 fn apply_window_style(window: &gtk::Window) {
     let css_provider = gtk::CssProvider::new();
