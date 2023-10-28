1603bc3b7e15e45d5b9e83f68348e8415de7f202
paulabruck <paulabruck@gmail.com>
2023-10-28 14:39:07 +0000

    warnings solved

--- src/gui/gui.rs
+++ src/gui/gui.rs
@@ -1,7 +1,5 @@
 use gtk::prelude::*;
 use gtk::Builder;
-use gtk::Window;
-use pango::Alignment;
 use std::sync::Mutex;
 
 pub static mut OPEN_WINDOWS: Option<Mutex<Vec<gtk::Window>>> = None;
@@ -10,19 +8,29 @@ pub fn run_main_window() {
     unsafe {
         OPEN_WINDOWS = Some(Mutex::new(Vec::new()));
     }
+
     let builder = Builder::new();
-    builder.add_from_file("src/gui/part3.ui");
+    match builder.add_from_file("src/gui/part3.ui") {
+        Ok(_) => {
+        },
+        Err(err) => {
+            eprintln!("Error loading the UI file: {}", err);
+        }
+    }
 
     let window: gtk::Window = builder.get_object("window").expect("Failed to get the window");
     window.set_default_size(800, 600);
     add_to_open_windows(&window);
+    apply_window_style(&window);
+
     let button_clone: gtk::Button = get_button(&builder, "buttonclone", "Clone");
     let button_init: gtk::Button = get_button(&builder, "buttoninit", "Init");
+    apply_button_style(&button_clone);
+    apply_button_style(&button_init);
 
-    apply_common_style(&button_clone, &button_init);
-    apply_window_style(&window);
-    connect_button_clicked(&button_clone, "Clone");
-    connect_button_clicked(&button_init, "Init");
+    connect_button_clicked_main_window(&button_clone, "Clone");
+    connect_button_clicked_main_window(&button_init, "Init");
+    
     window.show_all();
 }
 
@@ -31,25 +39,9 @@ pub fn close_all_windows() {
         if let Some(ref mutex) = OPEN_WINDOWS {
             let mut open_windows = mutex.lock().expect("Mutex lock failed");
             for window in open_windows.iter() {
-                window.close(); // Close each window
+                window.close(); 
             }
-            open_windows.clear(); // Clear the list
-        }
-    }
-}
-
-fn print_open_windows() {
-    unsafe {
-        if let Some(ref windows) = OPEN_WINDOWS {
-            if let Ok(windows) = windows.lock() {
-                for window in windows.iter() {
-                    println!("Window: {:?}", window);
-                }
-            } else {
-                println!("Failed to lock the window mutex");
-            }
-        } else {
-            println!("OPEN_WINDOWS is not initialized");
+            open_windows.clear(); 
         }
     }
 }
@@ -63,10 +55,16 @@ fn add_to_open_windows(window: &gtk::Window) {
     }
 }
 
-fn show_new_window() {
+fn show_repository_window() {
     let builder = gtk::Builder::new();
-    builder.add_from_file("src/gui/new_window.ui");
-
+    match builder.add_from_file("src/gui/new_window.ui") {
+        Ok(_) => {
+        },
+        Err(err) => {
+            eprintln!("Error loading the UI file: {}", err);
+        }
+    }
+    
     let new_window: gtk::Window = builder.get_object("window").expect("Failed to get the window");
     new_window.set_default_size(800, 600);
     apply_window_style(&new_window);
@@ -74,7 +72,7 @@ fn show_new_window() {
     new_window.show_all();
 }
 
-fn create_text_entry_window(button_type: &str, message: &str) {
+fn create_text_entry_window( message: &str) {
     let entry_window = gtk::Window::new(gtk::WindowType::Toplevel);
     add_to_open_windows(&entry_window);
     apply_window_style(&entry_window);
@@ -89,64 +87,80 @@ fn create_text_entry_window(button_type: &str, message: &str) {
     main_box.add(&entry);
 
     let ok_button = gtk::Button::with_label("OK");
-    apply_common_style(&ok_button, &ok_button);
+    apply_button_style(&ok_button);
     main_box.add(&ok_button);
 
     ok_button.connect_clicked(move |_| {
         let text = entry.get_text().to_string();
         println!("Entered Text: {}", text);
         close_all_windows();
-        show_new_window();
+        show_repository_window();
     });
 
     entry_window.show_all();
 }
 
-fn connect_button_clicked2(button: &gtk::Button, button_type: &str) {
+fn connect_button_clicked_init_window(button: &gtk::Button, button_type: &str) {
     let button_type = button_type.to_owned(); // Clone the label
 
     button.connect_clicked(move |_| {
         if button_type == "option2" {
-            create_text_entry_window(&button_type, "Enter the branch");
+            create_text_entry_window( "Enter the branch");
         } else if button_type == "option3" {
-            create_text_entry_window(&button_type, "Enter the template path");
+            create_text_entry_window( "Enter the template path");
         } else if button_type == "option1" {
             close_all_windows();
-            show_new_window();
+            show_repository_window();
         }
     });
 }
 
-fn connect_button_clicked(button: &gtk::Button, button_type: &str) {
-    let button_type = button_type.to_owned(); // Clone the label
+fn connect_button_clicked_main_window(button: &gtk::Button, button_type: &str) {
+    let button_type = button_type.to_owned(); 
 
     button.connect_clicked(move |_| {
         if button_type == "Init" {
             let builder_window_init = gtk::Builder::new();
-            builder_window_init.add_from_file("src/gui/windowInit.ui"); // Make sure the path is correct
+            match builder_window_init.add_from_file("src/gui/windowInit.ui") {
+                Ok(_) => {
+                },
+                Err(err) => {
+                    eprintln!("Error loading the UI file: {}", err);
+                }
+            }
+            
             let new_window_init: gtk::Window = builder_window_init.get_object("window").expect("Failed to get the window");
             add_to_open_windows(&new_window_init);
-            new_window_init.set_default_size(800, 600);
             apply_window_style(&new_window_init);
+            new_window_init.set_default_size(800, 600);
+            
             let button1: gtk::Button = get_button(&builder_window_init, "button1", "option1");
             let button2: gtk::Button = get_button(&builder_window_init, "button2", "option2");
             let button3: gtk::Button = get_button(&builder_window_init, "button3", "option3");
+            apply_button_style(&button1);
+            apply_button_style(&button2);
+            apply_button_style(&button3);
 
-            apply_common_style(&button2, &button1);
-            apply_common_style(&button3, &button1);
-
-            // Show the new "Init" window
             new_window_init.show_all();
-            connect_button_clicked2(&button1, "option1");
-            connect_button_clicked2(&button2, "option2");
-            connect_button_clicked2(&button3, "option3");
+
+            connect_button_clicked_init_window(&button1, "option1");
+            connect_button_clicked_init_window(&button2, "option2");
+            connect_button_clicked_init_window(&button3, "option3");
+
         } else if button_type == "Clone" {
             let builder_window_clone = gtk::Builder::new();
-            builder_window_clone.add_from_file("src/gui/windowClone.ui"); // Make sure the path is correct
+            match builder_window_clone.add_from_file("src/gui/windowClone.ui") {
+                Ok(_) => {
+                },
+                Err(err) => {
+                    eprintln!("Error loading the UI file: {}", err);
+                }
+            }
+                        
             let new_window_clone: gtk::Window = builder_window_clone.get_object("window").expect("Failed to get the window");
             add_to_open_windows(&new_window_clone);
-            new_window_clone.set_default_size(800, 600);
             apply_window_style(&new_window_clone);
+            new_window_clone.set_default_size(800, 600);
             new_window_clone.show_all();
         }
     });
@@ -174,7 +188,7 @@ fn get_button(builder: &Builder, button_id: &str, label_text: &str) -> gtk::Butt
     button
 }
 
-fn apply_common_style(button_clone: &gtk::Button, button_init: &gtk::Button) {
+fn apply_button_style(button: &gtk::Button) {
     let css_provider = gtk::CssProvider::new();
     css_provider
         .load_from_data("button {
@@ -186,9 +200,6 @@ fn apply_common_style(button_clone: &gtk::Button, button_init: &gtk::Button) {
         .as_bytes())
         .expect("Failed to load CSS");
 
-    let style_context_clone = button_clone.get_style_context();
-    style_context_clone.add_provider(&css_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
-
-    let style_context_init = button_init.get_style_context();
-    style_context_init.add_provider(&css_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
+    let style_context = button.get_style_context();
+    style_context.add_provider(&css_provider, gtk::STYLE_PROVIDER_PRIORITY_APPLICATION);
 }
