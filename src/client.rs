19b6dca5e40534a5e4ec1744999b6e0f352ef3f7
Ramiro Gestoso <rgestoso@fi.uba.ar>
2023-10-27 23:41:00 -0300

    agrego update_fetch y get_refs

--- src/client.rs
+++ src/client.rs
@@ -1,7 +1,9 @@
 use std::{
-    collections::HashMap,
-    io::{self, BufRead, BufReader, Read, Write, Error},
+    collections::{HashMap, btree_map::Entry},
+    fs,
+    io::{self, BufRead, BufReader, Error, Read, Write},
     net::TcpStream,
+    path::{Path, PathBuf},
     str::from_utf8,
     vec,
 };
@@ -14,6 +16,7 @@ const GIT_UPLOAD_PACK: &str = "git-upload-pack";
 
 #[derive(Debug)]
 struct Client {
+    git_dir: PathBuf,
     repository: String,
     host: String,
     socket: TcpStream,
@@ -24,20 +27,21 @@ struct Client {
 /// This is a git client that is able to connect to a git server
 /// using the git protocol.
 impl Client {
-
     /// Establish a connection with the server and asks for the refs in the remote.
     /// A hashmap with the path of the refs as keys and the last commit hash as values is returned.
-    /// 
+    ///
     /// Leaves the connection opened
     /// May fail due to I/O errors
-    pub fn get_refs(&mut self) -> io::Result<HashMap<String, String>> {
+    pub fn get_refs(&mut self) -> io::Result<Vec<String>> {
         self.upload_pack_initiate_connection()?;
         self.upload_pack_wait_refs();
-        Ok(self.refs.clone())
+
+        // let keys: Vec<&str> = self.refs.keys();
+        Ok(self.refs.keys().map(String::from).collect())
     }
 
     /// Creates a connection with a server (assuming its a git server)
-    /// 
+    ///
     /// Parameters:
     ///     - address: address to establish a tcp connection
     ///     - repository: name of the repository in the remote
@@ -50,6 +54,7 @@ impl Client {
         let host = host.to_owned();
         let capabilities = String::from("multi_ack_detailed side-band-64k wait-for-done");
         Ok(Self {
+            git_dir: PathBuf::new(),
             repository,
             host,
             socket,
@@ -74,36 +79,58 @@ impl Client {
     }
 
     pub fn end_connection(&mut self) -> io::Result<()> {
-        dbg!("FLUSHIIING");
         self.flush()
     }
     /// REVISAR: deberia ser como el upload-pack the git
-    // pub fn upload_pack(&mut self) -> io::Result<()> {
+    pub fn upload_pack(&mut self, wanted_ref: Option<&str>, git_dir: &str) -> io::Result<()> {
+        self.git_dir = PathBuf::from(git_dir);
+        self.upload_pack_initiate_connection()?;
+        self.upload_pack_wait_refs();
 
-    //     let _ = self.get_refs();
+        if let Some(wanted_ref) = wanted_ref {
+            if let Some(hash) = self.want_ref(wanted_ref)? {
+                self.update_fetch_head(&hash)?;
+            }
+            println!("Leo response de want");
+            self.read_response_until_flush();
+        }
+        // std::thread::sleep(std::time::Duration::from_secs(5));
 
-    //     self.upload_pack_send_wanted_refs()?;
-    //     // std::thread::sleep(std::time::Duration::from_secs(5));
-    //     self.upload_pack_initiate_connection()?;
-    //     println!("Leo response de want");
-    //     self.read_response_until_flush();
+        println!("Termino conexion");
+        self.flush()
+    }
 
-    //     println!("Termino conexion");
-    //     self.flush()
-    // }
+    fn update_fetch_head(&self, hash: &str) -> io::Result<()> {
+        let fetch_head = self.git_dir.join("FETCH_HEAD");
+        let mut fetch_head = fs::File::create(fetch_head)?;
+        writeln!(fetch_head, "{}", hash)
+    }
 
     /// Tells the server which refs are required
     /// If the provided ref name does not exist in remote, then an error is returned.
-    /// 
+    ///
     /// (DEBERIA TRAER EL PACKFILE PERO TODAVIA NO LO HACE)
-    pub fn want_ref(&mut self, wanted_ref: &str) -> io::Result<()> {
-
+    fn want_ref(&mut self, wanted_ref: &str) -> io::Result<Option<String>> {
         println!("Pido: {}", wanted_ref);
         // for wanted_ref in wanted_refs {
         let hash = match self.refs.get(wanted_ref) {
-            Some(hash) => hash,
-            None => return Err(Error::new(io::ErrorKind::NotFound, "Ref not found in remote")),
+            Some(hash) => hash.clone(),
+            None => {
+                return Err(Error::new(
+                    io::ErrorKind::NotFound,
+                    "Ref not found in remote",
+                ))
+            }
         };
+
+        let actual_refs = get_refs(&self.git_dir)?;
+
+        if let Some(actual_hash) = actual_refs.get(wanted_ref) {
+            if &hash == actual_hash {
+                println!("Already up to date.");
+                return Ok(None);
+            }
+        }
 
         let want = format!("want {} {}\n", hash, &self.capabilities);
         let want = pkt_line(&want);
@@ -117,7 +144,8 @@ impl Client {
         // despues de todos los have -> flush
         // self.send(PKT_FLUSH)?;
         // despues de todo -> DONE
-        self.done()
+        self.done()?;
+        Ok(Some(String::from(hash)))
 
         // }
         // Ok(())
@@ -125,9 +153,7 @@ impl Client {
 
     // Establish the first conversation with the server
     // Lets the server know that an upload-pack is requested
-    fn upload_pack_initiate_connection(
-        &mut self
-    ) -> io::Result<()> {
+    fn upload_pack_initiate_connection(&mut self) -> io::Result<()> {
         let mut command = format!("{} /{}", GIT_UPLOAD_PACK, self.repository);
 
         command = format!("{}\0host={}\0", command, self.host);
@@ -155,7 +181,7 @@ impl Client {
     }
 
     // Auxiliar function. Waits for the refs and loads them in self
-    // Should be called only aftes: upload_pack_initiate_connection 
+    // Should be called only aftes: upload_pack_initiate_connection
     fn upload_pack_wait_refs(&mut self) {
         let mut reader = BufReader::new(&self.socket);
         let _ = reader.read_line(&mut String::new());
@@ -175,9 +201,7 @@ impl Client {
         }
         dbg!(&self.refs);
     }
-
 }
-
 
 fn read_pkt_line(buf_reader: &mut BufReader<impl Read>) -> (usize, String) {
     let size = read_n_to_string(buf_reader, 4);
@@ -210,6 +234,19 @@ fn pkt_line(line: &str) -> String {
     len_hex + line
 }
 
+fn get_refs(git_dir: &PathBuf) -> io::Result<HashMap<String, String>> {
+    let mut refs = HashMap::new();
+    let heads = git_dir.join("refs/heads");
+    for entry in fs::read_dir(heads)? {
+        let entry = entry?;
+        let filename = entry.file_name().to_string_lossy().to_string();
+        let path = String::from("refs/heads") + &filename;
+        let hash = fs::read_to_string(&filename)?;
+        refs.insert(path, hash);
+    }
+    Ok(refs)
+}
+
 impl Drop for Client {
     fn drop(&mut self) {
         let _ = self.end_connection();
@@ -226,7 +263,7 @@ mod tests {
     #[ignore]
     fn test_get_refs() -> io::Result<()> {
         let address = "localhost:".to_owned() + PORT;
-        let mut client = Client::connect(&address, "repo","localhost")?;
+        let mut client = Client::connect(&address, "repo", "localhost")?;
         assert!(!client.get_refs()?.is_empty());
         // client.end_connection()
         Ok(())
@@ -237,8 +274,8 @@ mod tests {
     fn test_refs_has_head() -> io::Result<()> {
         let address = "localhost:".to_owned() + PORT;
         let mut client = Client::connect(&address, "repo", "localhost")?;
-        assert!(client.get_refs()?.contains_key("HEAD"));
-        // client.end_connection()
+        let refs = client.get_refs()?;
+        assert!(refs.contains(&"HEAD".to_string()));
         Ok(())
     }
 
@@ -248,11 +285,7 @@ mod tests {
         let address = "localhost:".to_owned() + PORT;
         let mut client = Client::connect(&address, "repo", "localhost")?;
         let refs = client.get_refs()?;
-        if let Some(wanted_ref) = refs.get("HEAD") {
-            client.want_ref(wanted_ref)?
-        }
+        client.want_ref(&refs[0])?;
         Ok(())
-        // assert!(client.get_refs()?.contains_key("HEAD"));
-        // Ok(())
     }
 }
