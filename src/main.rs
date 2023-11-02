e6fac755c1b2763a8cc35404ba76814d31e3573a
claram97 <cruano@fi.uba.ar>
2023-11-02 12:12:21 -0300

    commit?

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
+use messi::parse_commands::get_user_input;
 use std::fs;
 use std::io::Write;
-use messi::parse_commands::get_user_input;
 
-use messi::{add, commit};
 use messi::gui::run_main_window;
 use messi::parse_commands::{handle_git_command, parse_git_command};
+use messi::{add, commit};
 fn main() {
     if gtk::init().is_err() {
         println!("Failed to initialize GTK.");
@@ -27,5 +27,4 @@ fn main() {
     if let Some(git_command) = parse_git_command(second_argument) {
         handle_git_command(git_command, args);
     }
-
 }
