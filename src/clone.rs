44e99b4e2c0ff03bc48070a0ae4e46b1dfcd8f75
fe-martinez <femartinez@fi.uba.ar>
2023-11-04 23:51:36 -0300

    clone que deberia funcionar correctamente

--- src/clone.rs
+++ src/clone.rs
@@ -1,9 +1,9 @@
 use std::{
-    io::{self, Read},
+    io::{self, Read, Write},
     path::{Path, PathBuf},
 };
 
-use crate::{client::Client, init, tree_handler};
+use crate::{client::Client, init, tree_handler, branch, config};
 
 fn get_default_branch_commit(local_git_dir: &str) -> io::Result<String> {
     let path_to_file = local_git_dir.to_string() + "/refs/remotes/origin/master";
@@ -29,7 +29,6 @@ fn get_clean_refs(refs: Vec<String>) -> Vec<String> {
 
 fn get_parent_dir(path: &str) -> String {
     let path_buf = PathBuf::from(path);
-    // Do it without unwraps
     let parent = match path_buf.parent() {
         Some(parent) => parent,
         None => Path::new(""),
@@ -61,10 +60,32 @@ pub fn git_clone(
     }
     let default_branch_commit = get_default_branch_commit(&local_git_dir)?;
     let commit_tree = tree_handler::load_tree_from_commit(&default_branch_commit, &local_git_dir)?;
-    let parent_dir = get_parent_dir(&local_dir);
+    let parent_dir = &local_dir;
+    let branch_file_path = local_git_dir.to_string() + "/refs/heads/master";
+    let mut branch_file = std::fs::File::create(branch_file_path)?;
+    branch_file.write_all(default_branch_commit.as_bytes())?;
     commit_tree.create_directories(&parent_dir, &local_git_dir)?;
     let index_path = local_git_dir.to_string() + "/index";
     let gitignore_path = parent_dir.to_string() + "/.gitignore";
     commit_tree.build_index_file_from_tree(&index_path, &local_git_dir, &gitignore_path)?;
+    let mut config_file = config::Config::load(&local_git_dir)?;    
+    config_file.add_remote(remote_repo_name.to_string(), remote_repo_url.to_string(), remote_repo_url.to_string(), &mut io::stdout())?;
+    config_file.add_branch("master".to_string(), "origin".to_string(), "/refs/heads/master".to_string(), &mut io::stdout())?;
     Ok(())
 }
+
+pub fn git_fetch(remote_repo_url: &str, remote_repo_name: &str, host: &str, local_dir: &str) -> io::Result<()> {
+    let local_git_dir = local_dir.to_string() + "/.mgit";
+    let mut client = Client::new(&remote_repo_url, remote_repo_name, host);
+    let refs = client.get_refs()?;
+    let clean_refs = get_clean_refs(refs);
+
+    for server_ref in clean_refs {
+        let result = client.upload_pack(&server_ref, &local_git_dir, "origin");
+        if result.is_err() {
+            println!("Error: {:?}", result);
+        }
+    }
+    Ok(())
+}
+
