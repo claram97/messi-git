e9060a11ae180d1aead13916cc987f84b4de6dc6
claram97 <cruano@fi.uba.ar>
2023-11-05 19:54:52 -0300

    nuevo main

--- src/main.rs
+++ src/main.rs
@@ -1,33 +1,56 @@
-use std::io;
+use std::{env, io};
 
 use messi::gui::run_main_window;
 use messi::parse_commands::get_user_input;
 use messi::parse_commands::{handle_git_command, parse_git_command};
+
+fn run_with_gui() {}
+
+fn run_without_gui() {}
+
 fn main() -> io::Result<()> {
-    if gtk::init().is_err() {
+    let args: Vec<String> = env::args().collect();
+    if args.len() != 1 && args.len() != 2 {
         return Err(io::Error::new(
-            io::ErrorKind::Other,
-            "Failed to initialize GTK.",
+            io::ErrorKind::InvalidInput,
+            "Cantidad inválida de parámetros\n",
         ));
     }
 
-    run_main_window().map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
-
-    gtk::main();
-
-    let args = get_user_input();
-    let second_argument = match args.get(1) {
-        Some(arg) => arg,
-        None => {
+    if args.len() == 2 {
+        if args[1] != "gui" {
             return Err(io::Error::new(
-                io::ErrorKind::Other,
-                "No se ha ingresado el segundo argumento.",
+                io::ErrorKind::InvalidInput,
+                "Comando no reconocido\n",
             ));
         }
-    };
+        if gtk::init().is_err() {
+            return Err(io::Error::new(
+                io::ErrorKind::Other,
+                "Failed to initialize GTK.\n",
+            ));
+        }
 
-    if let Some(git_command) = parse_git_command(second_argument) {
-        handle_git_command(git_command, args);
+        run_main_window().map_err(|err| io::Error::new(io::ErrorKind::Other, err))?;
+
+        gtk::main();
+    }
+    else {
+        let args = get_user_input();
+        let second_argument = match args.get(1) {
+            Some(arg) => arg,
+            None => {
+                return Err(io::Error::new(
+                    io::ErrorKind::Other,
+                    "No se ha ingresado el segundo argumento.\n",
+                ));
+            }
+        };
+    
+        if let Some(git_command) = parse_git_command(second_argument) {
+            handle_git_command(git_command, args);
+        }
+    
     }
 
     Ok(())
