175f4c5258e3590e1d4dd1754bbfc600802dbf32
Ramiro Gestoso <rgestoso@fi.uba.ar>
2023-12-02 14:42:53 -0300

    update test

--- tests/tests_pull.rs
+++ tests/tests_pull.rs
@@ -1,4 +1,4 @@
-use messi::{pull::{self, git_pull}, init::git_init, remote::git_remote, git_config::git_config, config::Config};
+use messi::{pull::git_pull, init::git_init, remote::git_remote, config::Config};
 use std::{io, fs, path::Path};
 
 #[test]
@@ -11,17 +11,29 @@ fn remove_dir() -> io::Result<()> {
 
 #[test]
 #[ignore]
-fn test_pull() -> io::Result<()> {
+fn test_pull_empty_repo() -> io::Result<()> {
     let git_dir = "tests/pull";
-    let result = run_test(git_dir);
-    // fs::remove_dir_all(git_dir)?;
+    let result = first_pull(git_dir);
+    fs::remove_dir_all(git_dir)?;
     match result {
         Ok(_) => Ok(()),
         Err(e) => Err(e)
     }
 }
 
-fn run_test(git_dir: &str) -> io::Result<()> {
+#[test]
+#[ignore]
+fn test_pull_non_empty_repo() -> io::Result<()> {
+    let git_dir = "tests/pull";
+    let result = second_pull(git_dir);
+    fs::remove_dir_all(git_dir)?;
+    match result {
+        Ok(_) => Ok(()),
+        Err(e) => Err(e)
+    }
+}
+
+fn first_pull(git_dir: &str) -> io::Result<()> {
     git_init(git_dir, "master", None)?;
     let line = vec!["add", "origin", "localhost:9418/repo"];
     let repo_path = git_dir.to_owned() + "/.mgit";
@@ -29,12 +41,35 @@ fn run_test(git_dir: &str) -> io::Result<()> {
     git_remote(&mut config, line, &mut vec![])?;
     git_pull("master", git_dir, None, "localhost")?;
     
-    let files_count = fs::read_dir(repo_path)?.count();
-    if files_count < 7 {
-        return Err(io::Error::new(
-            io::ErrorKind::Other,
-            "Error: Files not copied\n",
-        ));
+    let has_file1 = fs::read_dir(git_dir)?.any(|x| x.unwrap().path() == Path::new(git_dir).join("hola.txt"));
+    let has_file2 = fs::read_dir(git_dir)?.any(|x| x.unwrap().path() == Path::new(git_dir).join("hola2.txt"));
+    
+    if !has_file1 || has_file2 {
+        return Err(io::Error::new(io::ErrorKind::Other, "Files copied incorrectly"));
     }
+
+    Ok(())
+}
+
+fn second_pull(git_dir: &str) -> io::Result<()> {
+    first_pull(git_dir)?;
+    let line = vec!["remove", "origin"];
+    let repo_path = git_dir.to_owned() + "/.mgit";
+
+    let mut config = Config::load(&repo_path)?;
+    git_remote(&mut config, line, &mut vec![])?;
+
+    let line = vec!["add", "origin", "localhost:9418/repoff"];
+    git_remote(&mut config, line, &mut vec![])?;
+
+    git_pull("master", git_dir, None, "localhost")?;
+    
+    let has_file1 = fs::read_dir(git_dir)?.any(|x| x.unwrap().path() == Path::new(git_dir).join("hola.txt"));
+    let has_file2 = fs::read_dir(git_dir)?.any(|x| x.unwrap().path() == Path::new(git_dir).join("hola2.txt"));
+
+    if !has_file1 || !has_file2 {
+        return Err(io::Error::new(io::ErrorKind::Other, "File copied incorrectly"));
+    };
+
     Ok(())
 }
